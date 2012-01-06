5bab1606834d4250efaeafbfdc8f03edd75c4b9f
Kristian Øllegaard <kristian.ollegaard@divio.ch>
2012-01-06 23:22:04 +0100

    function to upload ssl certificates, should maybe be called automatically on project.init if ssl = True

--- docs/public/project.rst
+++ docs/public/project.rst
@@ -7,3 +7,4 @@ Project layout and setup
 The following functions are available through the CLI, e.g. fab <dev/stage/live/...> <command>
 
 .. autofunction:: dploi_fabric.project.init
+.. autofunction:: dploi_fabric.project.upload_ssl
--- dploi_fabric/project.py
+++ dploi_fabric/project.py
@@ -1,12 +1,13 @@
 import os
 
-from fabric.api import env, task, prompt, run
+from fabric.api import env, task, prompt, run, put
 from fabric.contrib import files
 from .django_utils import django_exec
 from github import upload_ssh_deploy_key
 from supervisor import update_config_file as supervisor_update_config_file
 from nginx import update_config_file as nginx_update_config_file
 import django_utils
+from .utils import config
 @task
 def init():
     # TODO: Use utils.config (after checkout)
@@ -35,3 +36,15 @@ def init():
         django_utils.append_settings()
     supervisor_update_config_file()
     nginx_update_config_file()
+
+@task
+def upload_ssl():
+    """
+    Upload the SSL key and certificate to the directories and with the filenames
+    specified in your settings.
+    """
+    for site, site_dict in config.sites.items():
+        ssl_key_path = prompt("SSL Key path (%s):" % site)
+        ssl_cert_path = prompt("SSL Certificate path (%s):" % site)
+        put(ssl_key_path, site_dict.get("deployment").get("ssl_key_path"))
+        put(ssl_cert_path, site_dict.get("deployment").get("ssl_cert_path"))
