26d76b55796636f1a9d34463b0f4bf608276ec42
Benjamin Wohlwend <benjamin.wohlwend@divio.ch>
2012-01-13 18:15:27 +0100

    removed some dead code

--- dploi_fabric/memcache.py
+++
@@ -1,9 +0,0 @@
-SUPERVISOR_MEMCACHED_TEMPLATE = """
-[program:{{ user }}_memcached]
-command=memcached -s {{ project_path }}../tmp/memcached.sock
-directory={{ project_path }}
-user={{ user }}
-autostart=True
-autorestart=True
-redirect_stderr=True
-"""
