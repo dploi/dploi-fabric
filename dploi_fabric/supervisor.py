b4bca33770e83a7e9147c36faa196928db6c4110
Benjamin Wohlwend <piquadrat@gmail.com>
2014-02-18 11:33:33 +0100

    made the command which is called to update supervisor configurable

--- dploi_fabric/supervisor.py
+++ dploi_fabric/supervisor.py
@@ -6,8 +6,60 @@ from dploi_fabric.toolbox.template import render_template
 from dploi_fabric.utils import config
 import posixpath
 
+
 @task
-def update_config_file(dryrun=False):
+def stop():
+    for site, site_config in config.sites.items():
+        run('sudo supervisorctl stop %s:*' %  get_group_name(site, site_config))
+
+
+@task
+def start():
+    for site, site_config in config.sites.items():
+        run('sudo supervisorctl start %s:*' % get_group_name(site, site_config))
+
+
+@task
+def restart():
+    for site, site_config in config.sites.items():
+        run('sudo supervisorctl restart %s:*' % get_group_name(site, site_config))
+
+
+@task
+def status():
+    """
+    print status of the supervisor process
+
+    Note: "status" does not yet support the group syntax
+    """
+    for site, site_config in config.sites.items():
+        group_name = get_group_name(site, site_config)
+        for process_name, process_cmd in site_config.processes.items():
+            run('sudo supervisorctl status %s:%s' % (group_name, process_name))
+
+
+@task
+def add():
+    for site, site_config in config.sites.items():
+        group_name = get_group_name(site, site_config)
+        for process_name, process_cmd in site_config.processes.items():
+            run('sudo supervisorctl add %s:%s' % (group_name, process_name))
+
+
+@task
+def update():
+    for site, site_config in config.sites.items():
+        group_name = get_group_name(site, site_config)
+        for process_name, process_cmd in site_config.processes.items():
+            run('sudo supervisorctl update %s:%s' % (group_name, process_name))
+
+
+def get_group_name(site, site_config):
+    return '%s-%s' % (site_config['deployment']['user'], site)
+
+
+@task
+def update_config_file(dryrun=False, update_command=update):
     output = ''
     groups = {}
     for site, site_config in config.sites.items():
@@ -43,49 +95,4 @@ def update_config_file(dryrun=False):
         print output
     else:
         put(StringIO.StringIO(output), path)
-        update()
-
-@task
-def stop():
-    for site, site_config in config.sites.items():
-        run('sudo supervisorctl stop %s:*' %  get_group_name(site, site_config))
-
-
-@task
-def start():
-    for site, site_config in config.sites.items():
-        run('sudo supervisorctl start %s:*' % get_group_name(site, site_config))
-
-@task
-def restart():
-    for site, site_config in config.sites.items():
-        run('sudo supervisorctl restart %s:*' % get_group_name(site, site_config))
-
-@task
-def status():
-    """
-    print status of the supervisor process
-
-    Note: "status" does not yet support the group syntax
-    """
-    for site, site_config in config.sites.items():
-        group_name = get_group_name(site, site_config)
-        for process_name, process_cmd in site_config.processes.items():
-            run('sudo supervisorctl status %s:%s' % (group_name, process_name))
-
-@task
-def add():
-    for site, site_config in config.sites.items():
-        group_name = get_group_name(site, site_config)
-        for process_name, process_cmd in site_config.processes.items():
-            run('sudo supervisorctl add %s:%s' % (group_name, process_name))
-
-@task
-def update():
-    for site, site_config in config.sites.items():
-        group_name = get_group_name(site, site_config)
-        for process_name, process_cmd in site_config.processes.items():
-            run('sudo supervisorctl update %s:%s' % (group_name, process_name))
-
-def get_group_name(site, site_config):
-    return '%s-%s' % (site_config['deployment']['user'], site)
+        update_command()
